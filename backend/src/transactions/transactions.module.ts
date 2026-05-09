bf5c29d1c96493d7076fbdb7269d81521a82568f
riadhRCH <riadh@seekmake.tech>
2026-05-10 00:28:10 +0200

    fix buid issue

--- backend/src/transactions/transactions.module.ts
+++ backend/src/transactions/transactions.module.ts
@@ -4,6 +4,7 @@ import { TransactionsController } from './transactions.controller';
 import { TransactionsService } from './transactions.service';
 import { Transaction, TransactionSchema } from '../schemas/transaction.schema';
 import { Property, PropertySchema } from '../schemas/property.schema';
+import { Announcement, AnnouncementSchema } from '../schemas/announcement.schema';
 import { Lead, LeadSchema } from '../schemas/lead.schema';
 import { VisitRequest, VisitRequestSchema } from '../schemas/visit-request.schema';
 import { RentalAgency, RentalAgencySchema } from '../schemas/rental-agency.schema';
@@ -16,6 +17,7 @@ import { NotificationsModule } from '../notifications/notifications.module';
     MongooseModule.forFeature([
       { name: Transaction.name, schema: TransactionSchema },
       { name: Property.name, schema: PropertySchema },
+      { name: Announcement.name, schema: AnnouncementSchema },
       { name: Lead.name, schema: LeadSchema },
       { name: VisitRequest.name, schema: VisitRequestSchema },
       { name: RentalAgency.name, schema: RentalAgencySchema },
